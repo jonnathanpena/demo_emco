ea6a59c47a41c99ab12fe6aab2cd4e201681a8e2
Jonnathan Peña <razaisger@hotmail.com>
2018-09-26 12:29:35 -0500

    devextreme bien y con css

--- src/app/usuarios/crear/crear-usuario.component.ts
+++ src/app/usuarios/crear/crear-usuario.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit} from '@angular/core';
+import notify from 'devextreme/ui/notify';
 
 @Component({
   templateUrl: './crear-usuario.component.html',
@@ -6,8 +7,53 @@ import { Component, OnInit} from '@angular/core';
 })
 
 export class CrearUsuarioComponent implements OnInit {
+  usuario: any = {
+    de_id_persona: '',
+    de_nombre_per: '',
+    de_apellido_per: '',
+    de_celular_per: '',
+    de_correo_per: '',
+    de_provincia_per: '',
+    de_canton_per: '',
+    de_id_user: '',
+    de_usuario: '',
+    de_clave: '',
+    confirme: '',
+    de_persona_id: ''
+  };
+  provincias: any = [];
+  cantones: any = [];
 
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.usuario = {
+      de_id_persona: '',
+      de_nombre_per: '',
+      de_apellido_per: '',
+      de_celular_per: '',
+      de_correo_per: '',
+      de_canton_per: '',
+      de_id_user: '',
+      de_usuario: '',
+      de_clave: '',
+      confirme: '',
+      de_persona_id: ''
+    };
+    this.provincias = [
+      {
+        id_intprov: 0,
+        nombre_intprov: 'Machala'
+      }
+    ];
+    this.cantones = [
+      {
+        id_intcant: 0,
+        nombre_intcant: 'Canton 1',
+        prov_canton_id: 1
+      }
+    ];
+  }
+
+  crearUsuario() {}
 }
--- src/app/usuarios/usarios.module.ts
+++ src/app/usuarios/usarios.module.ts
@@ -7,11 +7,22 @@ import { UsuariosRoutes } from './usuarios.routing';
 import { CrearUsuarioComponent } from './crear/crear-usuario.component';
 import { ListarUsuariosComponent } from './listar/listar-usuarios.component';
 
+import {
+  DxTextBoxModule,
+  DxValidatorModule,
+  DxValidationSummaryModule,
+  DxSelectBoxModule
+} from 'devextreme-angular';
+
 @NgModule({
   imports: [
     CommonModule,
     RouterModule.forChild(UsuariosRoutes),
-    FormsModule
+    FormsModule,
+    DxTextBoxModule,
+    DxValidatorModule,
+    DxValidationSummaryModule,
+    DxSelectBoxModule
   ],
   declarations: [
     CrearUsuarioComponent,
